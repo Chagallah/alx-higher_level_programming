0eade78fa84fe0e8114ccc2b211c34e0f494e2b7
Chagallah <hchagalla@gmail.com>
2022-12-01 23:57:43 -0800

    Corrections

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     """
     from calculator_1 import add, sub, mul, div
     import sys
-    if len(sys.argv) -1 !=3:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     elif operator == '/':
         result = div(a, b)
     else:
-        print("Unknown operator. Available operators: + , - , * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
