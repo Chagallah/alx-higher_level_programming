7eeb11146600530e2f584fe3ff423dc9240ceace
Chagallah <hchagalla@gmail.com>
2023-01-04 13:03:24 -0800

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""add_integer module"""
+
+
+def add_integer(a, b=98):
+    """ add_integer function
+
+    this function adds two integer
+
+    Attributes:
+        a: first integer
+        b: optional second integer (init by 98 by default)
+    """
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,6 @@
+#Test-driven-develoment
+
+This repository contains task projects on:
+- Unit Tests in Python
+- Unittest module
+- Interactive and Non-interactivee tests
